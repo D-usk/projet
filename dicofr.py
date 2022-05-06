1a00967f91f4700ac3b4b546c679623d0fe1cc1d
ryu-demons <bapt33333@gmail.com>
2022-05-06 14:11:16 +0200

    projet début

--- dicofr.py
+++ dicofr.py
@@ -1,25 +1,24 @@
+
 mots_fr = []
 mots_cherches = []
 
 
-dicofr = open("dico_utf8.txt", "r")
-print(dicofr)
-"""liste_mots_fr = dicofr.readlines()
+dicofr = open("dicofr.txt", "r")
+liste_mots_fr = dicofr.readlines()
 for i in liste_mots_fr:
+    i.lower()
     mots_fr.append(i.replace("\n", ""))
-
 
 
 def recherche(dico):
     start = input("Premiere lettre du mot: ")
     longueur = input("Longueur du mot: ")
     lettres = input("lettres dans le(s) mot(s): ")
-    for i in dico:
-        if i[0] == start and len(i) == int(longueur) and lettres in i:
-            mots_cherches.append(i)
-        elif len(i) == int(longueur) and lettres in i:
-            mots_cherches.append(i)
+
+    for index in dico:
+        if index[0] == start and len(index) == int(longueur) and lettres in index:
+            mots_cherches.append(index)
 
 
 recherche(mots_fr)
-print(mots_cherches)"""
+print(mots_cherches)
