57f25490bd79cebded937fd0949009856da6a60e
ryu-demons <bapt33333@gmail.com>
2022-05-09 14:48:50 +0200

    recherche lettres et longueurs

--- dicofr.py
+++ dicofr.py
@@ -2,39 +2,46 @@ liste_mots_fr = []
 
 with open("dicofr.txt", "r") as line:
     for mot in line:
-        liste_mots_fr.append(mot.lower().replace("\n",""))
+        liste_mots_fr.append(mot.lower().replace("\n", ""))
 
-def recherche_positions(lettres, liste_mots_fr):
+
+def recherche_positions(lettres, liste_mot_fr):
     liste_valide = []
     total_lettres = len(lettres.keys())
-    for mot in liste_mots_fr:
+    for mots in liste_mot_fr:
         current_count = 0
         for key, value in lettres.items():
-            lettres_mot = { i:mot[i] for i in range(0,len(mot)-1) }
-            if lettres_mot.get(int(key)) == value:
+            lettres_mots = {i: mots[i] for i in range(0, len(mots)-1)}
+            if lettres_mots.get(int(key)) == value:
                 current_count += 1
 
         if current_count == total_lettres:
-            liste_valide.append(mot)
+            liste_valide.append(mots)
     return liste_valide
 
 
-def taille_mot():
+def taille_mot(liste_finale):
+    liste_valide_finale = []
+    longueur_mot = int(input("Entrer la longueur du mot: "))
+    for mots in liste_finale:
+        if len(mots) == longueur_mot:
+            liste_valide_finale.append(mots)
+    return liste_valide_finale
 
 
-print("Si vous connaissez une/plusieurs positions des lettres , veuillez les inscrires sous le format position:lettre (ex 0:a pour assez")
+print("Si vous connaissez une/plusieurs positions des lettres , veuillez les inscrires sous le format position:lettre (ex 0:a pour assez)\nEntrer \"ok\" quand vous avez fini: ")
 prem_lettres = input("lettre du mot: ")
 positions_lettre_connues = {}
 
 while prem_lettres != "ok":
     prem_lettres = prem_lettres.split(":")
-    if(prem_lettres is not None):
+    if prem_lettres is not None:
         positions_lettre_connues[prem_lettres[0]] = prem_lettres[1]
     prem_lettres = input("lettre du mot: ")
 
-##longueur = int(input("Longueur du mot: "))
 liste_valide_offset = recherche_positions(positions_lettre_connues, liste_mots_fr)
-print(liste_valide_offset)
+fin = taille_mot(liste_valide_offset)
+print(fin)
 """""
 for index in dico:
     if index[0] == start and len(index) == longueur and lettres in index:
@@ -45,4 +52,3 @@ for index in dico:
         mots_cherches.append(index)
     if index[0] == start and len(index) == longueur and lettres in index:
 """
-##print(mots_cherches)
